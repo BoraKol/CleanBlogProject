e339ed0a2ac4a879f434a8a284a62c0fc1546c10
Bora Kol <borakol07@gmail.com>
2024-03-02 18:40:36 +0300

    fourth commit

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.set("view engine" , "ejs");
+
+// const myLogger = (req,res,next) => {
+//     console.log("Middleware Log 1");
+//     next(); //diğer middleware geçebilmesi için yani req-res döngüsünün tamamlanabilmesi için next gerekli
+// }
+
+// const myLogger2 = (req,res,next) => {
+//     console.log("Middleware Log 2");
+//     next();
+// }
+
+app.use(express.static('public'));
+// app.use(myLogger);
+// app.use(myLogger2);
 
 app.get('/', function (req, res) {
 //   res.send('Hello World')
-    const blog = {
-        id:1,
-        title:'Blog title',
-        description: "Blog description",
-    }
+    // const blog = {
+    //     id:1,
+    //     title:'Blog title',
+    //     description: "Blog description",
+    // }
 
-    res.send(blog);
+    // res.send(blog);
+    res.render('index');
 })
 
 
